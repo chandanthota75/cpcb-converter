7741a282f57ef391f140e09616a7523d0bf2c4c3
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2022-05-07 18:24:15 +0530

    Add files via upload

--- cpcb_converter.py
+++ cpcb_converter.py
@@ -1,4 +1,5 @@
 import pandas as pa
+from io import BytesIO
 import streamlit as st
 
 
@@ -7,12 +8,17 @@ def main():
         page_title="CPCB file Processor"
     )
 
+    st.markdown('<style>h1, p, h3 {text-align: center;} div[class="row-widget stDownloadButton"] {text-align: c'
+                'enter;}</style>', unsafe_allow_html=True)
+
     st.title("CPCB file Processor")
 
     upload_file = st.file_uploader(label="Select the file", accept_multiple_files=False)
     if upload_file is not None:
         dataframe = pa.read_excel(upload_file, sheet_name="Sheet1")
-
+        st.subheader("")
+        st.success("The file uploaded successfully")
+        st.subheader("")
         dataframe.drop(dataframe.head(15).index, inplace=True)
         dataframe = (dataframe.astype(str).reset_index()).drop(["index"], axis=1)
 
@@ -34,7 +40,28 @@ def main():
         final_dataframe = pa.concat([first_half, second_half, third_half, fourth_half],
                                     axis=1, join='inner').reset_index().drop(["index"], axis=1)
 
-        final_dataframe.to_excel("cpcb_converted.xlsx", sheet_name='data', index=False)
+        def excel_file_converter(df):
+            output = BytesIO()
+            writer = pa.ExcelWriter(output, engine='xlsxwriter')
+            df.to_excel(writer, index=False, sheet_name='Sheet1')
+            worksheet = writer.sheets['Sheet1']
+            worksheet.set_column('A:A', None)
+            writer.save()
+            data_encoded = output.getvalue()
+            return data_encoded
+
+        df_xlsx = excel_file_converter(final_dataframe)
+        if df_xlsx:
+            name = st.text_input("Enter the file name with extension (.xlsx)")
+            if name:
+                st.subheader("")
+                st.success("The file is ready to download")
+                st.subheader("")
+                st.download_button(label='📥 Download the file',
+                                   data=df_xlsx,
+                                   file_name=f'{name}')
+        else:
+            st.info("The file is processing")
 
 
 if __name__ == "__main__":
